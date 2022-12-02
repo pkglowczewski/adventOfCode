ab3691465d2f98ce5d4651e42bf584712f61ea3e
Glowczewski <pkglowczewski@gmail.com>
2022-12-02 15:24:21 +0100

    add day 1 part 1

--- adventOfCode/Program.cs
+++ adventOfCode/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] text = File.ReadAllLines(@"C:\Users\x\source\repos\adventOfCode\adventOfCode\Resources\input_ex1.txt");
+int sum = 0;
+List<int> list = new List<int>();
+foreach(var item in text)
+{
+    if(item != "")
+    {
+        sum = sum + Convert.ToInt32(item);
+    }
+    else
+    {
+        list.Add(sum);
+        sum = 0;
+    }
+}
+var result = list.OrderByDescending(x => x).FirstOrDefault();
+Console.WriteLine(result);
+Console.ReadKey();
