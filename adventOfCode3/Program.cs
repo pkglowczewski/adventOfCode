98d493309d99bd0764306524308d767229cb8113
Glowczewski <pkglowczewski@gmail.com>
2022-12-03 15:01:19 +0100

    add day 3 part 2

--- adventOfCode3/Program.cs
+++ adventOfCode3/Program.cs
@@ -1,16 +1,22 @@
-﻿string[] text = File.ReadAllLines(@"C:\Users\x\source\repos\adventOfCode\adventOfCode3\Resources\input_ex3.txt");
+﻿var text = File.ReadAllLines(@"C:\Users\x\source\repos\adventOfCode\adventOfCode3\Resources\input_ex3.txt").ToList();
 int sum = 0;
-foreach(var line in text)
+while (text.Count > 0)
 {
-    var rucksackItems = line.Insert(line.Length / 2, " ").Split(" ");
-    var duplicateLetters= line.GroupBy(c => c).Where(g => g.Count() > 1);
+    var rucksackItems = text.Take(3).ToArray();
+    string allLetters = "";
+    foreach (var rucksackItem in rucksackItems)
+    {
+        allLetters += rucksackItem.ToString();
+    }
+    var duplicateLetters = allLetters.GroupBy(c => c).Where(g => g.Count() > 1);
     foreach (var item in duplicateLetters)
     {
-        if (rucksackItems[0].Contains(item.Key) && rucksackItems[1].Contains(item.Key))
+        if (rucksackItems[0].Contains(item.Key) && rucksackItems[1].Contains(item.Key) && rucksackItems[2].Contains(item.Key))
         {
-            sum += item.Key < 97 ? item.Key - 38 : item.Key - 96;
+            sum += Convert.ToInt32(item.Key) < 97 ? Convert.ToInt32(item.Key) - 38 : Convert.ToInt32(item.Key) - 96;
         }
     }
+    text.RemoveRange(0, 3);
 }
 Console.WriteLine(sum);
 Console.ReadKey();
