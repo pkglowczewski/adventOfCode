e90bde3f9c7361bb4cbe840e2bdd30ab882e264e
Glowczewski <pkglowczewski@gmail.com>
2022-12-03 08:27:00 +0100

    add day 2 part 2

--- adventOfCode2/Program.cs
+++ adventOfCode2/Program.cs
@@ -1,49 +1,60 @@
 ﻿string[] text = File.ReadAllLines(@"C:\Users\x\source\repos\adventOfCode\adventOfCode2\Resources\input_ex2.txt");
 int sum = 0;
 foreach (string line in text)
-{ 
-    sum += OutcomePoint(GetPlayerShape(line.Substring(0, 1)), GetPlayerShape(line.Substring(2, 1))) + GetPlayerShape(line.Substring(2, 1));
+{
+    var player1 = GetPlayerShape(line.Substring(0, 1));
+    var player2 = GetPlayer2Shape(player1, line.Substring(2, 1));
+    sum += player2;
 }
 
 Console.WriteLine(sum);
 Console.ReadKey();
 
-int OutcomePoint(int value, int value2)
-{
-    int difference = value - value2;
-    switch (difference)
-    {
-        case -1:
-        case 2:
-            return 6;
-        case -2:
-            return 0;
-        case 0:
-            return 3;
-        default:
-            return 0;
-    }
-}
-
 int GetPlayerShape(string letter)
 {
     string shape = "";
-    if (letter == "A" || letter == "X")
+    if (letter == "A")
     {
         shape = "ROCK";
     }
-    if (letter == "B" || letter == "Y")
+    if (letter == "B")
     {
         shape = "PAPER";
     }
-    if (letter == "C" || letter == "Z")
+    if (letter == "C")
     {
         shape = "SCISSORS";
     }
-    
     return (int)Enum.Parse(typeof(Shapes), shape);
 }
-
+int GetPlayer2Shape(int player1, string letter)
+{
+    int shape=0;
+    if (letter == "X")  // lose
+    {
+        if (player1 == 1)
+        {
+            player1 = 4;
+        }
+        shape = player1 - 1;
+        sum += 0;
+    }
+    if (letter == "Y") // draw
+    {
+        shape = player1;
+        sum += 3;
+    }
+    if (letter == "Z") // win
+    {
+        if (player1 == 3)
+        {
+            player1 = 0;
+        }
+        shape = player1 + 1;
+        sum += 6;
+    }
+    return shape;
+}
 enum Shapes
 {
     ROCK = 1,
