3db7132b4cfd9e8e33cb64dc88ed82d9d77a92f9
Glowczewski <pkglowczewski@gmail.com>
2022-12-04 22:10:19 +0100

    add day 4 part 1

---
+++ adventOfCode4/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] text = File.ReadAllLines(@"C:\Users\x\source\repos\adventOfCode\adventOfCode4\Resources\input_ex4.txt");
+int sum = 0;
+foreach (string line in text)
+{
+    var items = line.Split(',');
+    var firstSetfirstEl = Convert.ToInt32(items[0].Split('-')[0]);
+    var firstSetsecondEl = Convert.ToInt32(items[0].Split('-')[1]);
+    var secondSetfirstEl = Convert.ToInt32(items[1].Split('-')[0]);
+    var seocndSetsecondEl = Convert.ToInt32(items[1].Split('-')[1]);
+    if (firstSetfirstEl <= secondSetfirstEl && firstSetsecondEl >= seocndSetsecondEl || secondSetfirstEl <= firstSetfirstEl && firstSetsecondEl <= seocndSetsecondEl)
+    {
+        sum += 1;
+    }
+}
+Console.WriteLine(sum);
+Console.ReadKey();
